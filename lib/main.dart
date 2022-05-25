2b421626155cd0cd849eb8bb658d949c3798eb19
Piyush Malhotra <piyush@soulber.co>
2022-05-25 15:11:06 -0400

    add main front end

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[800],
+        appBar: AppBar(
+          title: const Text('Dice Roller'),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            // to fill in properly in a device
+            child: TextButton(
+              child: Image.asset('images/dice1.png'),
+              onPressed: () {}, // VoidCallback
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice3.png'),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
